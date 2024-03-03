0a222bdc9c4860721bd8290bb23369ac56243839
escuderodev <escudero.ti@gmail.com>
2024-03-03 14:35:56 -0300

    feat: paramos aos 12:30 da aula https://web.dio.me/course/o-que-e-o-typescript/learning/0f530f9b-6172-4177-a649-401b7347cb67?back=/track/formacao-typescript-fullstack-developer&tab=undefined&moduleId=undefined

--- Fundamentos/app.ts
+++ Fundamentos/app.ts
@@ -1,12 +1,40 @@
-var a: string = 'a' // global
-let b: string = 'b' // escopo
-const c: string = 'c' // constante
+// // variáveis
+// var a: string = 'a' // global
+// let b: string = 'b' // escopo
+// const c: string = 'c' // constante
 
-console.log(a,b,c)
+// console.log(a,b,c)
 
-const nome: string = 'Eduardo'
-const sobrenome: string = 'Escudero'
-const idade: number = 39
-let situacao: boolean = true
+// const nome: string = 'Eduardo'
+// const sobrenome: string = 'Escudero'
+// const idade: number = 39
+// let situacao: boolean = true
 
-console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos. Será que eu estou ativo no curso? ${situacao}`)
+// console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos. Será que eu estou ativo no curso? ${situacao}`)
+
+// objetos
+
+// definindo os tipos dos atributos de um objeto via interface
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: string //o ? indica opcional
+}
+
+// declarando um objeto respeitando os tipos da interface
+const pessoa: Pessoa = {
+    nome: 'Eduardo',
+    idade: 39
+}
+
+const outraPessoa: Pessoa = {
+    nome: 'Carol',
+    idade: 40,
+    profissao: 'Confeiteira'
+}
+
+const arrayPessoa: Array<Pessoa> = [pessoa, outraPessoa]
+console.log(arrayPessoa)
+
+const arrayNumerico: Array<number> = [1,2,3]
+console.log(arrayNumerico)
