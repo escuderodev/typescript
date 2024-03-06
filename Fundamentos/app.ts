3364a33eead8b616d1f2ef4489c018613b5d0e0c
escuderodev <escudero.ti@gmail.com>
2024-03-05 21:38:42 -0300

    feat: próxima aula Estruturas de Repetição

--- Fundamentos/app.ts
+++ Fundamentos/app.ts
@@ -1,4 +1,4 @@
-// // variáveis
+// =====  variáveis ===== 
 // var a: string = 'a' // global
 // let b: string = 'b' // escopo
 // const c: string = 'c' // constante
@@ -12,29 +12,71 @@
 
 // console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos. Será que eu estou ativo no curso? ${situacao}`)
 
-// objetos
+// ===== objetos ===== 
 
-// definindo os tipos dos atributos de um objeto via interface
-interface Pessoa {
-    nome: string,
-    idade: number,
-    profissao?: string //o ? indica opcional
+// // definindo os tipos dos atributos de um objeto via interface
+// interface Pessoa {
+//     nome: string,
+//     idade: number,
+//     profissao?: string //o ? indica opcional
+// }
+
+// // declarando um objeto respeitando os tipos da interface
+// const pessoa: Pessoa = {
+//     nome: 'Eduardo',
+//     idade: 39
+// }
+
+// const outraPessoa: Pessoa = {
+//     nome: 'Carol',
+//     idade: 40,
+//     profissao: 'Confeiteira'
+// }
+
+// const arrayPessoa: Array<Pessoa> = [pessoa, outraPessoa]
+// console.log(arrayPessoa)
+
+// const arrayNumerico: Array<number> = [1,2,3]
+// console.log(arrayNumerico)
+
+// ===== estrutura de decisão e repetição ===== 
+// const numero: number = 15
+
+// if(numero > 15) {
+//     console.log(`Número ${numero} é maior que 15`)
+// } else if(numero === 15) {
+//     console.log(`Número ${numero} é igual à 15`)
+// } else {
+//     console.log(`Número ${numero} é menor que 15`)
+// }
+
+// encadeamento de decisão
+// const complementoDaMovimentacao: boolean = true
+// const advogadoMarcado: boolean = true
+// const parteReveuMarcada: boolean = false
+
+// if(complementoDaMovimentacao) {
+//     if(advogadoMarcado) {
+//         console.log('Publica no DJE')
+//     } else if(parteReveuMarcada) {
+//         console.log('Publica no DJE')
+//     } else {
+//         console.log('Não possui advogado ou partes marcados então Não Publica')
+//     }
+// } else {
+//     console.log('Não possui complemento da movimentação então Não Publica')
+// }
+
+const typeUser = {
+    admin: 'você é um admin',
+    student: 'você é um estudante',
+    viewer: 'você é um espectador'
 }
 
-// declarando um objeto respeitando os tipos da interface
-const pessoa: Pessoa = {
-    nome: 'Eduardo',
-    idade: 39
+function validateUser(user: string) {
+    console.log(typeUser[user as keyof typeof typeUser])
 }
 
-const outraPessoa: Pessoa = {
-    nome: 'Carol',
-    idade: 40,
-    profissao: 'Confeiteira'
-}
+const usuario = 'admin'
 
-const arrayPessoa: Array<Pessoa> = [pessoa, outraPessoa]
-console.log(arrayPessoa)
-
-const arrayNumerico: Array<number> = [1,2,3]
-console.log(arrayNumerico)
+validateUser(usuario)
