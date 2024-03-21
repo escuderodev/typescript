61aa5d6fc7c8966bc8242ade3fcbae75e2234f3d
escuderodev <escudero.ti@gmail.com>
2024-03-21 20:20:00 -0300

    feat: paramos em 14:22 - https://www.youtube.com/watch?v=95sAtAareR8&ab_channel=BoniekyLacerda

---
+++ to_do_list/to_do_list_react/src/App.tsx
@@ -0,0 +1,14 @@
+import * as Sty from './App.styles'
+
+const App = () => {
+  return (
+    <Sty.Container>
+      <Sty.Area>
+      <Sty.Header>Lista de Tarefas</Sty.Header>
+      </Sty.Area>
+    </Sty.Container>
+  );
+}
+
+
+export default App;
