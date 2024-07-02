8ddebee5edc12a05e8626fdd898f09e66a8353e1
escuderodev <escudero.ti@gmail.com>
2024-07-01 21:24:56 -0300

    feat: att

--- customer-api-typescript/src/index.ts
+++ customer-api-typescript/src/index.ts
@@ -3,3 +3,6 @@ import { App } from "./App";
 new App().getServer().listen(3000)
 
 console.log('Server is running on http://localhost:3000')
+
+// Paramos no vídeo: https://www.youtube.com/watch?v=6l4uXfxb3Jc&list=PLrnHpYM1USWz9JMdtsbpPDYDNUuP_cpK_&index=6&ab_channel=andrevitor103
+// Aos 18:06
