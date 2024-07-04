32be0511fbe074b1532fe17e3404ea769f2226b7
escuderodev <escudero.ti@gmail.com>
2024-07-04 10:17:46 -0300

    feat: att project

--- usuarios-api/src/server.js
+++ usuarios-api/src/server.js
@@ -1,12 +1,37 @@
-import express from "express"
+import express, { json } from "express"
 import "dotenv/config"
 
 const app = express()
 
-app.listen(process.env.PORT)
+const users = []
 
-console.log(`Server is running on http://localhost:${process.env.PORT}`)
+const PORT = process.env.PORT
 
-app.get("/usuarios", (req, res) => {
-    res.status(200).send('Bem vindo a minha API!')
+app.listen(PORT)
+
+app.use(json())
+
+console.log(`Server is running on http://localhost:${PORT}`)
+
+// criar usuário
+app.post("/usuarios", (req, res) => {
+    const usuario = {
+        nome: req.body.nome,
+        email: req.body.email,
+        password: req.body.password
+    }
+    users.push(usuario)
+    res.status(200).json({usuario})
 })
+
+
+// listar todos os usuários
+app.get("/usuarios", (req, res) => {
+    res.status(200).json({users})
+})
+
+// listar apenas um usuário
+
+// atualizar usuário
+
+// deletar usuário
