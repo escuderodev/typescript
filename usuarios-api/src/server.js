3ef72ab085ba2185495f97068b1ff4f4ee448710
escuderodev <escudero.ti@gmail.com>
2024-07-04 15:06:11 -0300

    feat: method get and post implemented

--- usuarios-api/src/server.js
+++ usuarios-api/src/server.js
@@ -1,5 +1,8 @@
 import express, { json } from "express"
 import "dotenv/config"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 const app = express()
 
@@ -14,24 +17,35 @@ app.use(json())
 console.log(`Server is running on http://localhost:${PORT}`)
 
 // criar usuário
-app.post("/usuarios", (req, res) => {
+app.post("/usuarios", async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
     const usuario = req.body
-    users.push(usuario)
-    res.status(200).json({usuario})
+    res.status(201).json(usuario)
 })
 
 
 // listar todos os usuários
-app.get("/usuarios", (req, res) => {
-    res.status(200).json({users})
+app.get("/usuarios", async (req, res) => {
+    const userList = await prisma.user.findMany()
+    res.status(200).json({userList})
 })
 
 // listar apenas um usuário
-app.get("/usuarios/:id", (req, res) => {
-    const usuario = users.filter(user => users.find(u => u.id === req.params.id))
-    res.status(200).json({usuario})
-})
+// app.get("/usuarios/:id", (req, res) => {
+//     const usuario = users.filter(user => users.find(u => u.id === req.params.id))
+//     res.status(200).json({usuario})
+// })
 
 // atualizar usuário
 
 // deletar usuário
+
+
+// https://www.youtube.com/watch?v=PyrMT0GA3sE&ab_channel=DevClub%7CPrograma%C3%A7%C3%A3o
