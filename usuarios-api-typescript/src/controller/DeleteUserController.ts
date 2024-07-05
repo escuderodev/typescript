66ca71cc7a5d91e24e2d842c6282bbcf07cc3164
escuderodev <escudero.ti@gmail.com>
2024-07-05 20:54:42 -0300

    feat: att

--- usuarios-api-typescript/src/controller/DeleteUserController.ts
+++ usuarios-api-typescript/src/controller/DeleteUserController.ts
@@ -5,10 +5,5 @@ export const deleteUser = async (req: Request, res: Response) => {
 
     const deleteUserService = new DeleteUserService()
     await deleteUserService.execute(req)
-    // await prisma.user.delete({
-    //     where: {
-    //         id: req.params.id
-    //     }
-    // })
     return res.status(200).json({message: 'Usuário deletado com sucesso!'})
 }
