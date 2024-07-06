a385c761c014d66173363be7aa1219b370511266
escuderodev <escudero.ti@gmail.com>
2024-07-06 11:15:23 -0300

    feat: att

--- usuarios-api-typescript/src/controller/UpdateUserController.ts
+++ usuarios-api-typescript/src/controller/UpdateUserController.ts
@@ -4,7 +4,17 @@ import { UpdateUserService } from "../service/UpdateUserService"
 export const updateUser = async (req: Request, res: Response) => {
 
     const updateUserService = new UpdateUserService()
-    const user = await updateUserService.execute(req)
-    return res.status(200).json(user)
+    // const user = await updateUserService.execute(req)
+    // return res.status(200).json(user)
+
+    try {
+        const user = await updateUserService.execute(req)
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(200).json({
+            message: `User Id ${req.params.id} not found!`,
+            erro: `${error}`
+          })
+    }
 
 }
