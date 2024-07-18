5dd19952aae3979f212e5114c05038da38cda577
escuderodev <escudero.ti@gmail.com>
2024-07-18 18:20:58 -0300

    feat: in refatoration

--- postech-fase2-projeto-mongoose/src/model/User.ts
+++ postech-fase2-projeto-mongoose/src/model/User.ts
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     // id: {type: String},
     name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true, select: false},
     createdAt: {type: Date, default: Date.now()},
     updatedAt: {type: Date}
 })
