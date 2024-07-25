23044f87fd365419605eab2ab4f212ddf9d86c5b
EscuderoDev <escuderodev@gmail.com>
2024-07-25 13:31:43 -0300

    feat: att

--- customer-api-typescript/src/controller/dtos/CustomerUpdateDTO.ts
+++
@@ -1,4 +0,0 @@
-export class CustomerUpdateDTO {
-    constructor(readonly name: string, readonly document: string) {
-    }
-}
--- customer-api-typescript/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { App } from "./app"
-import "dotenv/config"
-
-new App().getServer().listen(process.env.PORT)
