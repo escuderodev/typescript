4d637cf12db4269205874f1ac184621b187f1252
escuderodev <escudero.ti@gmail.com>
2024-08-11 18:42:50 -0300

    feat: create class Person

--- FormacaoTypeScriptFullstackDeveloper/poo/src/app.ts
+++ FormacaoTypeScriptFullstackDeveloper/poo/src/app.ts
@@ -1,1 +1,10 @@
-console.log("Teste")
+import { Person } from "./models/Person"
+
+const eduardo = new Person("Eduardo Escudero", 40)
+const carol = new Person("Carol Tobias", 41)
+const persons: Array<Person> = []
+persons.push(eduardo, carol)
+
+console.log(persons)
+console.log(eduardo.getName())
+console.log(carol.getAge())
