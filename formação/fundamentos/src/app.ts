9132457cb7a34ff15a702b50556df740819efcce
escuderodev <escudero.ti@gmail.com>
2024-08-11 15:18:34 -0300

    feat: trabalhando com arrays e tipos

--- formação/fundamentos/src/app.ts
+++ formação/fundamentos/src/app.ts
@@ -1,5 +1,22 @@
-const soma = (num1: number, num2: number) => {
-    console.log(num1 + num2)
+interface Pessoa {
+    name: string
+    age: number
+    profession?: string
 }
 
-soma(10, 5)
+const eduardo: Pessoa = {
+    name: "Eduardo",
+    age: 40,
+    profession: "Dev"
+}
+
+const carol: Pessoa = {
+    name: "Carol",
+    age: 41
+}
+
+console.log(`Pessoa 1 = Nome: ${eduardo.name} | Idade: ${eduardo.age} | Profissão: ${eduardo.profession}`)
+console.log(`Pessoa 1 = Nome: ${carol.name} | Idade: ${carol.age}`)
+
+const pessoas: Array<Pessoa> = [eduardo, carol]
+console.log(pessoas)
