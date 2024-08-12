cbab62c43ef9cf2888d4e237d3c4b0ad34198ffd
escuderodev <escudero.ti@gmail.com>
2024-08-12 11:36:11 -0300

    feat: att

--- FormacaoTypeScriptFullstackDeveloper/desafio1/src/app.ts
+++ FormacaoTypeScriptFullstackDeveloper/desafio1/src/app.ts
@@ -1,7 +1,13 @@
-const nomes: Array<String> = ["Eduardo", "Carol", "Juan", "Alícia", "Valmir", "Fátima", "Cida", "Daniel", "Sônia", "Anselmo"]
+// const nomes: Array<String> = ["Eduardo", "Carol", "Juan", "Alícia", "Valmir", "Fátima", "Cida", "Daniel", "Sônia", "Anselmo"]
 
-for(let i = 0; i < nomes.length; i++) {
-    if(i === 2 || i === 6 || i === 8) {
-        console.log(nomes[i])
-    }
-}
+// for(let i = 0; i < nomes.length; i++) {
+//     if(i === 2 || i === 6 || i === 8) {
+//         console.log(nomes[i])
+//     }
+// }
+
+let M: number = 47
+let A: number = 21
+let B: number = 9
+let C: number = M - (A + B)
+console.log(C)
