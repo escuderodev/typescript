a3026157503582f38e9b95c5e6c896c1669391c5
Eduardo Escudero <escudero.ti@gmail.com>
2024-08-13 21:51:45 -0300

    feat: att

--- FormacaoTypeScriptFullstackDeveloper/IntroduçãoaoReactComTypescript/my-app-typescript/src/App.tsx
+++ FormacaoTypeScriptFullstackDeveloper/IntroduçãoaoReactComTypescript/my-app-typescript/src/App.tsx
@@ -1,30 +1,25 @@
 import { Card } from "./components/Card";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <div>
-        Primeiro App React com TypeScript
-        <Card
-          id={1}
-          title="TypeScript"
-          description="Como inserir tipagem no JavaScript"
-        />
-        <Card
-          id={2}
-          title="MongoDB"
-          description="Banco de dados não relacional."
-        />
-        <Card 
-          id={3} 
-          title="Express" 
-          description="Crie API's REST." />
-      </div>
-      <Footer message="Desenvolvido por Escuderodev" year="2024"/>
-    </>
+    <Layout>
+      Primeiro App React com TypeScript
+      <Card
+        id={1}
+        title="TypeScript"
+        description="Como inserir tipagem no JavaScript"
+      />
+      <Card
+        id={2}
+        title="MongoDB"
+        description="Banco de dados não relacional."
+      />
+      <Card 
+        id={3} 
+        title="Express" 
+        description="Crie API's REST." />
+    </Layout>
   );
 }
 
