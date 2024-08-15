e83475a2c993e7314b68dfe3f627fdcda7ccee0b
Eduardo Escudero <escudero.ti@gmail.com>
2024-08-15 17:50:56 -0300

    feat: paramos na aula Chakra UI

--- FormacaoTypeScriptFullstackDeveloper/IntroduçãoaoReactComTypescript/diobank/src/App.tsx
+++ FormacaoTypeScriptFullstackDeveloper/IntroduçãoaoReactComTypescript/diobank/src/App.tsx
@@ -1,24 +1,20 @@
-import { Card } from "./components/Card/Card";
 import { Layout } from "./components/Layout/Layout";
+import styled from "styled-components";
+
+const Box = styled.div`
+  background-color: orange;
+  padding-left: 5px;
+`
 
 function App() {
   return (
     <Layout>
-      Primeiro App React com TypeScript
-      <Card
-        id={1}
-        title="TypeScript"
-        description="Como inserir tipagem no JavaScript"
-      />
-      <Card
-        id={2}
-        title="MongoDB"
-        description="Banco de dados não relacional."
-      />
-      <Card 
-        id={3} 
-        title="Express" 
-        description="Crie API's REST." />
+      <Box><h1>Faça Login</h1></Box>
+      <label htmlFor="email">Email:</label>
+      <input type="email" name="email" id="email" placeholder="seuemail@provedor.com.br" required/>
+      <label htmlFor="password">Senha:</label>
+      <input type="password" name="password" id="password" required/>
+      <button type="submit">Entrar</button>
     </Layout>
   );
 }
