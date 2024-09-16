03d695aa16855f6ba3c37e99d05bf85f80cd7031
Eduardo Escudero <escuderodev@outlook.cm>
2024-09-16 16:26:10 -0300

    feat: cimplificando a declaração da classe e seu construtor

--- fundamentos/parte_1/app/models/negociacao.ts
+++ fundamentos/parte_1/app/models/negociacao.ts
@@ -1,14 +1,19 @@
 export class Negociacao {
-    private _data: Date
-    private _quantidade: number
-    private _valor: number
+    // // forma padrão
+    // private _data: Date
+    // private _quantidade: number
+    // private _valor: number
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data
-        this._quantidade = quantidade
-        this._valor = valor
-    }
-
+    // constructor(data: Date, quantidade: number, valor: number) {
+    //     this._data = data
+    //     this._quantidade = quantidade
+    //     this._valor = valor
+    // }
+    
+    // forma simplificada
+    // se necessário, poderia ter um atributo diferente aqui
+    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
+ 
     get data(): Date {
         return this._data
     }
